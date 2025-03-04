268f5990c7a2f3adea2fdc1d9abfe185609088d8
Martin-M-G <martin.githinji.maina@gmail.com>
2025-03-04 22:53:06 +0300

    New developments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
-      <div className='container'>
+      <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav></nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/Watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
       </div>
 
       <Footer></Footer>
